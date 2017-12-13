a83734b9bbdb74957929145a260e7e04b35a3889
woyllyn@qq.com <woyllyn@qq.com>
2017-12-13 11:15:44 +0800

    debug

--- send.go
+++ send.go
@@ -53,10 +53,10 @@ func (s *Server) SendText(to string, agentId int, msg string, safe ...int) (e *W
 	n := leng/500 + 1
 
 	if n == 1 {
-		return s.SendMsg(NewText(to, agentId, msg))
+		return s.SendMsg(s.NewText(to, agentId, msg))
 	} else {
 		for i := 0; i < n; i++ {
-			e = s.SendMsg(NewText(to, agentId, fmt.Sprintf("%s\n(%v/%v)", Substr(msg, i*500, (i+1)*500), i+1, n)))
+			e = s.SendMsg(s.NewText(to, agentId, fmt.Sprintf("%s\n(%v/%v)", Substr(msg, i*500, (i+1)*500), i+1, n)))
 		}
 	}
 	return
@@ -79,47 +79,47 @@ func Substr(str string, start int, end int) string {
 
 // SendImage 发送客服Image消息
 func (s *Server) SendImage(to string, id int, mediaId string) *WxErr {
-	return s.SendMsg(NewImage(to, id, mediaId))
+	return s.SendMsg(s.NewImage(to, id, mediaId))
 }
 
 // SendVoice 发送客服Voice消息
 func (s *Server) SendVoice(to string, id int, mediaId string) *WxErr {
-	return s.SendMsg(NewVoice(to, id, mediaId))
+	return s.SendMsg(s.NewVoice(to, id, mediaId))
 }
 
 // SendFile 发送客服File消息
 func (s *Server) SendFile(to string, id int, mediaId string) *WxErr {
-	return s.SendMsg(NewFile(to, id, mediaId))
+	return s.SendMsg(s.NewFile(to, id, mediaId))
 }
 
 // SendVideo 发送客服Video消息
 func (s *Server) SendVideo(to string, id int, mediaId, title, desc string) *WxErr {
-	return s.SendMsg(NewVideo(to, id, mediaId, title, desc))
+	return s.SendMsg(s.NewVideo(to, id, mediaId, title, desc))
 }
 
 // SendTextcard 发送客服extcard消息
 func (s *Server) SendTextcard(to string, id int, title, desc, url string) *WxErr {
-	return s.SendMsg(NewTextcard(to, id, title, desc, url))
+	return s.SendMsg(s.NewTextcard(to, id, title, desc, url))
 }
 
 // SendMusic 发送客服Music消息
 func (s *Server) SendMusic(to string, id int, mediaId, title, desc, musicUrl, qhMusicUrl string) *WxErr {
-	return s.SendMsg(NewMusic(to, id, mediaId, title, desc, musicUrl, qhMusicUrl))
+	return s.SendMsg(s.NewMusic(to, id, mediaId, title, desc, musicUrl, qhMusicUrl))
 }
 
 // SendNews 发送客服news消息
 func (s *Server) SendNews(to string, id int, arts ...Article) *WxErr {
-	return s.SendMsg(NewNews(to, id, arts...))
+	return s.SendMsg(s.NewNews(to, id, arts...))
 }
 
 // SendMpNews 发送加密新闻mpnews消息(仅企业号可用)
 func (s *Server) SendMpNews(to string, id int, arts ...MpArticle) *WxErr {
-	return s.SendMsg(NewMpNews(to, id, arts...))
+	return s.SendMsg(s.NewMpNews(to, id, arts...))
 }
 
 // SendMpNewsId 发送加密新闻mpnews消息(直接使用mediaId)
 func (s *Server) SendMpNewsId(to string, id int, mediaId string) *WxErr {
-	return s.SendMsg(NewMpNewsId(to, id, mediaId))
+	return s.SendMsg(s.NewMpNewsId(to, id, mediaId))
 }
 
 // SendMsg 发送消息

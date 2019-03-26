104806aea36f00f527bc934c06b6624f1cc1823c
woylyn@qq.com <woylyn@qq.com>
2019-03-26 14:35:23 +0800

    debug

--- corp_checkin.go
+++ corp_checkin.go
@@ -59,7 +59,7 @@ func (s *Server) GetCheckIn(opType, start, end int64, Namelist []string) (dkdata
 func (s *Server) GetAllCheckIn(opType, start, end int64) (dkdata []DkData, err error) {
 	ul := s.GetUserIdList()
 	l := len(ul)
-	for i := 0; i*100 < l; i++ {
+	for i := 0; i < l; i += 100 {
 		dk, e := s.GetCheckIn(opType, start, end, ul[i:min(l, i+100)])
 		if e != nil {
 			err = e
